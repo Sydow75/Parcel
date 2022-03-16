e6f6c6fad1c51c7d35cbba0042a726fe474b7c57
Michaela Sydow <91529037+Sydow75@users.noreply.github.com>
2022-03-16 13:45:17 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,14 @@ const imgUrl = new URL('./img/bild.jpg', import.meta.url)
 console.log(imgUrl);
 img.src = imgUrl.href;
 //måste skapa en ny fil ts config för att ovan skall funka. annars funkar inte import.meta.url se steg för steg i claras underlag
+
+
+
+//const imgNames = ['./img/bild.jpg', './img/katt2.jpg', './img.katt3.jpg'];
+
+/*for (let i:number= 0; i<imgNames.length; i++) {
+    const img = document.createElement('img');
+    document.body.append(img);
+    const imgUrl = new URL(`./img/${el}.jpg`, import.meta.url)
+    img.src = imgUrl.href
+}*/
